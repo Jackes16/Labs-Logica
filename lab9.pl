93f6de59b8f09021b9231c2e5359e408be5785f9
JakesQDLC <jakes1608@gmail.com>
2025-11-20 16:53:56 -0500

    Add files via upload

---
+++ lab9.pl
@@ -0,0 +1,14 @@
+sentence --> noun_phrase, place_phrase, complement_phrase.
+noun_phrase --> determiner, adjective, noun, verb.
+complement_phrase --> complement, determiner, adjective, noun.
+place_phrase --> determiner_place, determiner, place.
+
+determiner --> [the] |[a].
+noun --> [cat]|[dog]|[fish].
+verb --> [eats]|[sees]|[plays].
+adjective --> [happy]|[hungry].
+determiner_place -->[in].
+place --> [church]|[park].
+places --> []|place, places.
+adjectives -->[]|adjective. adjectives.
+complement --> [with].
